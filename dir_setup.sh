e04fa768861f78f616297652815beb1248845d74
mat <matdallen@gmail.com>
2023-12-22 09:30:17 -0800

    run on full directory

--- dir_setup.sh
+++ dir_setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# cosma folder setup
-
-# import creds
 source .creds
 
+# TODO: check if this overwrites
 # doesn't seem like this overwrites, just returns an error (file exists)
 mkdir ./data/ ./export/ # will this overwrite if already exists? TODO check that and then handle if so
 
@@ -13,15 +11,23 @@ if ! [[ -f "./config.yml" ]]; then
     cosma config
 fi
 
-## configure the configuration
-sed -i 's|directory|csv|' ./config.yml # using "|" because I've got directory strucutes going on.
-sed -i 's|nodes_origin: ""|nodes_origin: "./data/nodes.csv"|' ./config.yml
-sed -i 's|links_origin: ""|links_origin: "./data/links.csv"|' ./config.yml
-sed -i 's|history: true|history: false|' ./config.yml
-sed -i 's|export_target: ""|export_target: "./export/"|' ./config.yml
+## configure the configuration: CSV STYLE
+# sed -i 's|directory|csv|' ./config.yml # using "|" because I've got directory strucutes going on.
+# sed -i 's|nodes_origin: ""|nodes_origin: "./data/nodes.csv"|' ./config.yml
+# sed -i 's|links_origin: ""|links_origin: "./data/links.csv"|' ./config.yml
+# sed -i 's|history: true|history: false|' ./config.yml
+# sed -i 's|export_target: ""|export_target: "./export/"|' ./config.yml
 
-# cosma modelize
-# firefox export/cosmoscope.html
+## configure the configuration: MARKDOWN STYLE
+sed -i 's|files_origin: ""|files_origin: "./data/"|' ./config.yml
+sed -i 's|export_target: ""|export_target: "./export/"|' ./config.yml
+sed -i 's|history: true|history: false|' ./config.yml
+sed -i 's|generate_id: always|generate_id: never|' ./config.yml # not sure if this is needed or not.
+
+
+
+cosma modelize
+firefox export/cosmoscope.html
 
 cd ./export/
 ftp -n -p $FTP_SERVER <<END_SCRIPT
--- main.py
+++ main.py
@@ -2,41 +2,54 @@ import os
 import csv
 import datetime
 from openai import OpenAI
-# import instructor
-# import subprocess
 from typing import List
 from bs4 import BeautifulSoup
-# from pydantic import BaseModel, Field
 
 OpenAI.api_key_path = "/home/mat/Documents/ProgramExperiments/openAIapiKey"
 client = OpenAI()
-
-# instructor.patch(openai)
-# nodes.csv and links.csv
-
 BLOG_DIR = "/home/mat/Documents/blog/blog/"
 
+###### CSV CREATION SECTION: LINKS NOT WORKING SO NOT BEING USED #########
 
 ### Create csv files if they don't exist ###
-# TODO: (not sure if this sentence makes sense) if the file exists ask if they want to overwrite it, if not then append to it
-# check if nodes.csv exists, if not create it with fields title,id,type:primary,tag:<name>,meta:<name>,time:begin,content,thumbnail,reference
-if not os.path.exists("./data/nodes.csv"):
-    with open("./data/nodes.csv", "w") as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(["title", "id", "type:primary", "tag:<name>", "meta:<name>", "time:begin", "content", "thumbnail", "reference"])
+# # check if nodes.csv exists, if not create it with fields title,id,type:primary,tag:<name>,meta:<name>,time:begin,content,thumbnail,reference
+# if not os.path.exists("./data/nodes.csv"):
+#     with open("./data/nodes.csv", "w") as csv_file:
+#         csv_writer = csv.writer(csv_file)
+#         csv_writer.writerow(["title", "id", "type:primary", "tag:<name>", "meta:<name>", "time:begin", "content", "thumbnail", "reference"])
 
-# check if links.csv exists, if not create it with fields id,source,target,label
-if not os.path.exists("./data/links.csv"):
-    with open("./data/links.csv", "w") as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(["id", "source", "target", "label"])
+# # check if links.csv exists, if not create it with fields id,source,target,label
+# if not os.path.exists("./data/links.csv"):
+#     with open("./data/links.csv", "w") as csv_file:
+#         csv_writer = csv.writer(csv_file)
+#         csv_writer.writerow(["id", "source", "target", "label"])
 
 
 # get all blog posts from /home/mat/Documents/blog/blog/ and put them into a list
 blog_list = os.listdir(BLOG_DIR)
 
+def create_yaml(blog_name, model_res):
+
+    content = f"""---
+title: {blog_name}.md
+types:
+- undefined
+---
+
+{model_res}
+
+Link to the original post: https://enjoy.monster/blog/{blog_name}.html
+"""
+
+    with open(f"./data/{blog_name}.md", "w") as md_file:
+        md_file.write(content)    
+    return
+
+    
+
 for post in blog_list:
     raw_blog = open(BLOG_DIR + post, "r").read()
+    print(f"working on {post}")
 
     parsed_blog = BeautifulSoup(raw_blog, 'html.parser').get_text()
 
@@ -47,8 +60,10 @@ for post in blog_list:
         "role": "system",
         "content": f"""you are the world's greatest assistant go you chat :) 
         Please take the following blog post and construct a three to four sentence summary of the content and topics of the post.
-        Write this as if you were the author of the post, saying "I" and "me" and "my" as opposed to "the author" or "the writer".
-        If a post references a another post of the following: {blog_list} then please link to it in the summary in the format [[name_of_linked_post]]
+        Write this as if you were the author of the post and quite excited about the post's contents taking the tone of a random presenter, a cowboy, Shakespeare, a robot, anything! Anything you want to do to make it fun and interesting.
+        Emojis are HIGHLY encouraged! And please make sure to put links to other websites in the summary if they are referenced in the post. 
+        Make sure to say "I" and "me" as opposed to "the author" or "the writer", have this be your voice.
+        If a post references a another post of the following: {blog_list} then please link to it in the summary in the format [[name-of-linked-post.md]] (note the replacing of '_' with '-')
         """
         },
         {
@@ -57,10 +72,13 @@ for post in blog_list:
         }
     ])
 
-    # time of creation as id of node
-    current_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    base_name = post.split("/")[-1].split(".")[0].replace("_", "-")
 
-    # write the response to a csv file
-    with open("./data/nodes.csv", "a") as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow([post.split("/")[-1], post.split("/")[-1], "", "", "", "", response.choices[0].message.content, "", ""])
+    create_yaml(base_name, response.choices[0].message.content)
+    # raw_blog.close()
+
+    #### WRITE TO CSV: NOT USED UNTIL LINKS ARE WORKED OUT ####
+    # # write the response to a csv file
+    # with open("./data/nodes.csv", "a") as csv_file:
+    #     csv_writer = csv.writer(csv_file)
+    #     csv_writer.writerow([post.split("/")[-1], post.split("/")[-1], "", "", "", "", response.choices[0].message.content, "", ""])
